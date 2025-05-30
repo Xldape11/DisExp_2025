901b705b3d3c3d71907d938a5dc1f1eee5d184e2
Xldape11 <andyramosabril004@gmail.com>
2025-05-30 12:55:28 -0600

    Add files via upload

---
+++ Lab1.2.R
@@ -0,0 +1,67 @@
+# Laboratorio 1
+# Erik Manuel Aldape Becerra
+# 20/03/25
+
+# Parte II Variables
+
+# Problema 1
+# Identifique el tipo de variable(cualitativa o cuantitativa) de preguntas de una encuesta:
+
+# Nombre del estudiante: Cualitativa
+## Fecha de nacimiento: Cuantitativa
+## Edad: Cuantitativa
+## Dirección de casa: Cualitativa
+## Número de telefono: Cuantitativa
+## Área principal de estudio: Cualititiva
+## Grado de año universitario: Cuantitativa
+## Puntaje de la prueba de mitad de periodo: Cuantitativa
+## Calificacion General A, B, C, D, F: Cualitativa
+## Tiempo (en minutos) para completar la prueba final de MCF 202: Cuantitativa
+## Numero de hermanos: Cuantitativa
+
+# Problema 2
+# Elija un objeto y obtenga una lista de 4 variables: 2 cuantitativas y 2 categoricas.
+
+## Cuantitativas
+### Altura de un árbol
+### Edad de un árbol
+
+## Categoricas
+### Especie
+### Estado de conservación
+
+# Problema 3
+# Considere una variable con valores numéricos que describen formas electrónicas de expresar opiniones.
+
+## 1= Twitter; 2= correo electrónico; 3= mensaje de texto; 4= Facebook; 5= Blog
+### Todas esas son variables cualitativas porque representan categorías de medios de comunicación y no cantidades.
+
+# Problema 4
+# Para cada pregunta de investigación
+## Identifique los individuos de interes
+## Identifique las variables
+## Determine si cada variable es categórico o cuantitativo
+
+# ¿Cuál es la cantidad promedio de horas que los estudiantes de universidad púbilcas trabajan cada semana?
+## Estudiantes de universidad pública
+## Promedio de horas de trabajo
+## Horas <- c(8, 10, 12, 14, 16, 18)
+## Variable cuantitativas
+
+# ¿ Qué proporción de todos los estudiantes universitarios de México están inscritos en una universidad publica?
+## Estudiantes de universidad pública
+## Estudiantes inscritos en una universidad pública
+## Cuantitativa
+
+# En las universidades publicas , ¿las estudiantes femeninas tienen un promedio de CENEVAL más alto que los estudiantes varones?
+## Estudiantes femeninas y varones
+## Promedio de CENEVAL
+## Cuantitativa
+
+# ¿Es mas probable que los atletas universitarios reciban asesoramiento académico que los atletas no universitarios?
+## Atletas universitarios y No universitarios
+## Probabilidad de asesoramiento académico
+## Categórico
+
+# Si reunimos datos para responder a las preguntas de la investigación, ¿qué datos podrian analizarse mediante un histograma? ¿Cómo lo sabes?
+## Cada pregunta podria analizarse mediante un histograma ya que puede representar muy bien los datos que proporciona cada pregunta
